bfe9f874c5c9bf148d86579e279f4938a107120c
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-06-26 16:03:33 +0100

    Add a script to create a checklist This is for GitHub issue https://github.com/fnic-lemmy/bestofbot/issues/7

---
+++ scripts/checklist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import re
+import json
+
+with open('directory.json', 'r') as json_file:
+  sections = json.load(json_file)
+
+for s in sections:
+  print(f'- [ ] {s}')
+  for ss in sections[s]:
+    if ss != "None":
+      print(f'   - [ ] {ss}')
+      for sss in sections[s][ss]:
+        if sss != "None":
+          print(f'      - [ ] {sss}')
