8e3075e5fbe27ad26672e03d7e7883b1c6b0225d
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-07-12 14:13:19 +0100

    Add args

--- scripts/lemmy_settings_modify.py
+++ scripts/lemmy_settings_modify.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
 import json
+import argparse
 
-lfile='./lemmy_export.json'
+def get_args():
+  parser = argparse.ArgumentParser(description="Lemmy follows/blocks",
+             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("-i", "--input-file", help="Lemmy export file", default="lemmy_export.json")
+  parser.add_argument("-o", "--output-file", help="Modified file to output", default="lemmy_modified.json")
+  parser.add_argument("-f", "--follows-file", help="Follows list")
+  parser.add_argument("-b", "--blocks-file", help="Blocks list")
+  parser.add_argument("-F", "--wipe-follows", help="Wipes out follows before adding", action="store_true")
+  parser.add_argument("-B", "--wipe-blocks", help="Wipes out blocks before adding", action="store_true")
+  args = parser.parse_args()
+  a = vars(args)
 
-blks='./blocks.json'
-flws='./follows.json'
+  return(a)
+
+
+a = get_args()
+lfile=a['input_file']
+ofile=a['output_file']
+
+blks=a['blocks_file']
+flws=a['follows_file']
 
 wipe_flws = False
+if a['wipe_follows'] is True:
+  wipe_flws = True
+
 wipe_blks = False
+if a['wipe_blocks'] is True:
+  wipe_blks = True
 
 with open(lfile) as l:
   settings = json.load(l)
@@ -33,3 +56,6 @@ settings['blocked_communities'].extend(blocks)
 print('----------------')
 print(settings['followed_communities'])
 print(settings['blocked_communities'])
+
+with open(ofile, 'w') as outfile:
+  json.dump(settings, outfile)
