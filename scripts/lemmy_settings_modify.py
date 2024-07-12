f8e1cfadc444767340b07978f1e25feb24e9b6a2
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-07-12 14:55:48 +0100

    Accept export files generalted by directory_to_json.py Converts them to the correct URL format for the export file

--- scripts/lemmy_settings_modify.py
+++ scripts/lemmy_settings_modify.py
@@ -16,6 +16,23 @@ def get_args():
 
   return(a)
 
+def open_convert_list(file):
+  new = []
+  with open(file) as f:
+    comms = json.load(f)
+
+  if len(comms) > 0:
+    for cm in comms:
+      cms = cm.split('@')
+      if cms[1] in ['kbin.social', 'fedia.io']:
+        c = 'm'
+      else:
+        c = 'c'
+      new.append(f'https://{cms[1]}/{c}/{cms[0]}')
+  return new
+
+new_follows = []
+new_blocks = []
 
 a = get_args()
 lfile=a['input_file']
@@ -35,25 +52,20 @@ if a['wipe_blocks'] is True:
 with open(lfile) as l:
   settings = json.load(l)
 
-#print(settings['followed_communities'])
-#print(settings['blocked_communities'])
-
 if wipe_flws is True:
   settings['followed_communities'] = []
 
 if wipe_blks is True:
   settings['blocked_communities'] = []
 
-with open(flws) as f:
-  follows = json.load(f)
+if flws is not None:
+  follows = open_convert_list(flws)
+  settings['followed_communities'].extend(follows)
 
-with open(blks) as b:
-  blocks = json.load(b)
+if blks is not None:
+  blocks = open_convert_list(blks)
+  settings['blocked_communities'].extend(blocks)
 
-settings['followed_communities'].extend(follows)
-settings['blocked_communities'].extend(blocks)
-
-print('----------------')
 print(f'New follows: {settings["followed_communities"]}')
 print(f'New blocks: {settings["blocked_communities"]}')
 
